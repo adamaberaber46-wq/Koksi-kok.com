2fdecaf48f84ab821940205e92beb24f11e0d1e4
adamaber1212 aber <adamaberaber46@gmail.com>
2025-10-22 09:23:27 +0000

    عايز السعر يكون بالانجليزي

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatPrice(price: number) {
-  return new Intl.NumberFormat('ar-EG', {
+  return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'EGP',
   }).format(price);
