52a6bf9ad2a3f7b60cf37f934205362bbc676715
adamaber1212 aber <adamaberaber46@gmail.com>
2025-10-22 09:53:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2403957249-989bc",
+  "appId": "1:461218439:web:f8fc9e435607ffb96d8e22",
+  "apiKey": "AIzaSyC8RqqvwA3on1lbOTaFr-p8DCl06uymiLQ",
+  "authDomain": "studio-2403957249-989bc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "461218439"
+};
