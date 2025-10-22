0b916f8fd7610433676c6aa9abcc428fcfdd119c
adamaber1212 aber <adamaberaber46@gmail.com>
2025-10-22 09:54:48 +0000

    File changes

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,7 +1,12 @@
 'use client';
 
 import { CartProvider } from '@/context/cart-context';
+import { FirebaseClientProvider } from '@/firebase';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <CartProvider>{children}</CartProvider>;
+  return (
+    <FirebaseClientProvider>
+      <CartProvider>{children}</CartProvider>
+    </FirebaseClientProvider>
+  );
 }
